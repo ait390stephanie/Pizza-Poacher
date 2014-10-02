033336da07c041c1d9d9afe6199d1196c9278a61
ait390stephanie <ait390stephanie@users.noreply.github.com>
2014-10-02 14:13:44 -0400

    Create DeletePizzaServlet.java

---
+++ DeletePizzaServlet.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2014 -
+ * Licensed under the Academic Free License version 3.0
+ * http://opensource.org/licenses/AFL-3.0
+ * 
+ * Authors: Stephanie Olson & Tetis Chang
+ */
+package PizzaPoacher.gae.servlet;
+ 
+import java.io.IOException;
+ 
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import PizzaPoacher.gae.db.Pizzerias;
+
+@SuppressWarnings("serial")
+public class DeletePizzaServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		boolean result=Pizzerias.deletePizzaCommand(req.getParameter("pizzaID")); //"pizzaID assuming that each pizza ordered will be given an identification num"
+		resp.setStatus((result)?HttpServletResponse.SC_OK:HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	}
+}
